f63e0abf849c60d01473aa4aed673f980e2b8bfb
NJORO <karugunjoroge@gmail.com>
2019-08-13 22:55:30 +0300

    made changes in login.html

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY='lalala'
-# export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
+export SECRET_KEY='drip'
+export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
 
 python3.6 manage.py server
